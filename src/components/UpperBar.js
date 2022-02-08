15481ba41657fcfeb798c52c9300cd7072c29232
karinmat <62010666+karinmat@users.noreply.github.com>
2022-02-08 15:25:53 +0200

    logo image and style

--- src/components/UpperBar.js
+++ src/components/UpperBar.js
@@ -1,7 +1,11 @@
+import logo from "../images/spotify_logo.JPG";
+
 export default function UpperBar() {
     return (
         <div className="UpperBar">
-            <div className="logo">Spotify</div>
+            <div className="logo">
+                <img src={logo} alt="spotify"></img>
+            </div>
         </div>
     );
 }
