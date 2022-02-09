4bad98fa542dbd7500412959de7e91a42031befa
karinmat <62010666+karinmat@users.noreply.github.com>
2022-02-09 12:40:07 +0200

    fotter and info aligned

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -2,11 +2,13 @@ import Player from "./Player";
 
 export default function PlayerBar({ currentSong }) {
     return (
-        <div className="PlayerBar">
-            <div className="CurrentSongInfo" style={{ textAlign: "left" }}>
-                current song is: {currentSong}
+        <div className="Footer">
+            <div className="PlayerBar">
+                <div className="CurrentSongInfo" style={{ textAlign: "left" }}>
+                    current song is: {currentSong}
+                </div>
+                <Player />
             </div>
-            <Player />
         </div>
     );
 }
